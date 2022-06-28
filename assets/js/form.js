bee7a057f9334776f0c840019e3e501f5bf4dcf1
Ronnel Jones <ronneljones@icloud.com>
2022-06-28 09:54:12 -0700

    Add files via upload

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,22 +2,6 @@
 
 
 //FORM
-const form = document.querySelector('.contact-form')
-const name = document.querySelector('#form-name')
-const email = document.querySelector('#form-email')
-const subject = document.querySelector('#form-subject')
-const message = document.querySelector('#form-mess')
-
-
-Email.send({
-    name: document.getElementById('form-name').value,
-    Host : "smtp.zoho.com",
-    Username : "contact@ronnel.codes",
-    Password : "!1stWord1!",
-    To : 'contact@ronnel.codes',
-    From : document.getElementById('form-email').value,
-    Subject : document.getElementById('form-subject').value,
-    Body : document.getElementById('form-mess').value
-  }).then(
-    message => alert(message)
-    );
+(function(){
+    emailjs.init("i9dIOs8WCBGlf8Mmx");
+ })();
